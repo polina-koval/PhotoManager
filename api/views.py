806b7633b87a1288449cb6db86c191bab6f63d95
Polina Koval <zagadka1316@gmail.com>
2022-12-14 16:43:14 +0300

    Add RegisterAPI and LoginAPI.

--- api/views.py
+++ api/views.py
@@ -1,13 +1,17 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, login
 from django_filters.rest_framework import DjangoFilterBackend
+from knox.models import AuthToken
+from knox.views import LoginView as KnoxLoginView
 from rest_framework import status, viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.generics import GenericAPIView
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 
 from api.serializers import (
     PhotoDetailSerializer,
     PhotoListSerializer,
-    UserSerializer,
+    UserSerializer, RegisterSerializer,
 )
 from photo.filters import PhotoFilter
 from photo.models import Photo
@@ -20,8 +24,28 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
 
 
-class PhotoSerializer:
-    pass
+class RegisterAPI(GenericAPIView):
+    serializer_class = RegisterSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response({
+        "user": UserSerializer(user, context=self.get_serializer_context()).data,
+        "token": AuthToken.objects.create(user)[1]
+        })
+
+
+class LoginAPI(KnoxLoginView):
+    permission_classes = (AllowAny, )
+
+    def post(self, request, format=None):
+        serializer = AuthTokenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        login(request, user)
+        return super(LoginAPI, self).post(request, format=None)
 
 
 class PhotoViewSet(viewsets.ModelViewSet):
