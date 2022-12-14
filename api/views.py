e2c5a86df8d02267276a6b99d94f58f7cea8be04
Polina Koval <zagadka1316@gmail.com>
2022-12-14 13:04:00 +0300

    Filter queryset for user.

--- api/views.py
+++ api/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 from api.serializers import PhotoSerializer, UserSerializer
 from photo.models import Photo
@@ -15,3 +16,10 @@ class UserViewSet(viewsets.ModelViewSet):
 class PhotoViewSet(viewsets.ModelViewSet):
     queryset = Photo.objects.all()
     serializer_class = PhotoSerializer
+    permission_classes = [IsAuthenticated, ]
+
+    def get_queryset(self):
+        user = self.request.user
+        if user:
+            return Photo.objects.filter(user__pk=user.pk)
+
