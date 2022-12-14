e72ff7d7a1f28aeb08e3378f477ec3815ef7ef07
Polina Koval <zagadka1316@gmail.com>
2022-12-14 15:08:24 +0300

    Add ArrayField in admin.

--- photo/admin.py
+++ photo/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
+from django_better_admin_arrayfield.admin.mixins import DynamicArrayMixin
 
 from photo.models import Photo
 
 
 @admin.register(Photo)
-class PhotoAdmin(admin.ModelAdmin):
+class PhotoAdmin(admin.ModelAdmin, DynamicArrayMixin):
     list_display = ["user", "description", "people"]
