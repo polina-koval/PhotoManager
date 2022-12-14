354cb13d7988ccc38a7bdeaa21ea0bf102218d6a
Polina Koval <zagadka1316@gmail.com>
2022-12-14 21:33:46 +0300

    Reformat code.

--- api/views.py
+++ api/views.py
@@ -1,18 +1,22 @@
 from django.contrib.auth import get_user_model, login
 from django_filters.rest_framework import DjangoFilterBackend
+from drf_yasg.utils import swagger_auto_schema
 from knox.models import AuthToken
 from knox.views import LoginView as KnoxLoginView
 from rest_framework import status, viewsets
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.generics import GenericAPIView
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.parsers import MultiPartParser
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 
 from api.serializers import (
     PhotoDetailSerializer,
     PhotoListSerializer,
-    UserSerializer, RegisterSerializer,
+    RegisterSerializer,
+    UserSerializer,
 )
+from api.swagger_parameters import request_body_login
 from photo.filters import PhotoFilter
 from photo.models import Photo
 
@@ -31,15 +35,20 @@ class RegisterAPI(GenericAPIView):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
-        return Response({
-        "user": UserSerializer(user, context=self.get_serializer_context()).data,
-        "token": AuthToken.objects.create(user)[1]
-        })
+        return Response(
+            {
+                "user": UserSerializer(
+                    user, context=self.get_serializer_context()
+                ).data,
+                "token": AuthToken.objects.create(user)[1],
+            }
+        )
 
 
 class LoginAPI(KnoxLoginView):
-    permission_classes = (AllowAny, )
+    permission_classes = (AllowAny,)
 
+    @swagger_auto_schema(request_body=request_body_login)
     def post(self, request, format=None):
         serializer = AuthTokenSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -56,6 +65,7 @@ class PhotoViewSet(viewsets.ModelViewSet):
     ]
     filter_backends = (DjangoFilterBackend,)
     filterset_class = PhotoFilter
+    parser_classes = (MultiPartParser,)
 
     def get_queryset(self):
         user = self.request.user
@@ -68,6 +78,9 @@ class PhotoViewSet(viewsets.ModelViewSet):
         return PhotoListSerializer
 
     def create(self, request, *args, **kwargs):
+        """
+        Upload photo, can add metadata
+        """
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
