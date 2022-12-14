b8d327a2b28b236b870b22fa345606a406681ba3
Polina Koval <zagadka1316@gmail.com>
2022-12-14 16:42:55 +0300

    Add RegisterSerializer.

--- api/serializers.py
+++ api/serializers.py
@@ -12,6 +12,18 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ["url", "username", "email", "is_staff"]
 
 
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(validated_data['username'], validated_data['email'], validated_data['password'])
+
+        return user
+
+
 class PhotoListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Photo
