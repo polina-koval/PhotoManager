67ac77e6276af8f93454788ce8daad473b8a83bc
Polina Koval <zagadka1316@gmail.com>
2022-12-14 13:15:29 +0300

    Add filterset_fields in PhotoViewSet.

--- api/views.py
+++ api/views.py
@@ -1,3 +1,4 @@
+from django_filters import rest_framework as filters
 from django.contrib.auth import get_user_model
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
@@ -17,6 +18,8 @@ class PhotoViewSet(viewsets.ModelViewSet):
     queryset = Photo.objects.all()
     serializer_class = PhotoSerializer
     permission_classes = [IsAuthenticated, ]
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_fields = ['location', 'date', "people"]
 
     def get_queryset(self):
         user = self.request.user
