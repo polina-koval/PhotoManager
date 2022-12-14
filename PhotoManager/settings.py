05c7a6786a73d3a8c282ada3094101e1a3693218
Polina Koval <zagadka1316@gmail.com>
2022-12-14 20:13:51 +0300

    Add TEST db to config.

--- PhotoManager/settings.py
+++ PhotoManager/settings.py
@@ -84,13 +84,16 @@ TEMPLATES = [
 WSGI_APPLICATION = "PhotoManager.wsgi.application"
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_PORT"),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
+        "TEST": {
+            "NAME": "test_photo_db",
+        },
     }
 }
 
