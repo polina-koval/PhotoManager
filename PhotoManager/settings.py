fff2782cd0bd13d1276100a2709254d261f3a826
Polina Koval <zagadka1316@gmail.com>
2022-12-15 19:58:58 +0300

    Add defaults for postgres config.

--- PhotoManager/settings.py
+++ PhotoManager/settings.py
@@ -86,11 +86,11 @@ WSGI_APPLICATION = "PhotoManager.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
+        "NAME": os.getenv("DB_NAME", default="db_photo"),
+        "USER": os.getenv("DB_USER", default="postgres_user"),
+        "PASSWORD": os.getenv("DB_PASSWORD", default="password"),
+        "HOST": os.getenv("DB_HOST", default="127.0.0.1"),
+        "PORT": os.getenv("DB_PORT", default="5432"),
         "TEST": {
             "NAME": "test_photo_db",
         },
