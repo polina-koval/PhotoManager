602cb25f10b55e7d177c6070b0edab1ea6101633
Polina Koval <zagadka1316@gmail.com>
2022-12-15 18:41:09 +0300

    Add get_preview_img to PhotoAdmin.

--- photo/admin.py
+++ photo/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 from django_better_admin_arrayfield.admin.mixins import DynamicArrayMixin
 
 from photo.models import Photo
@@ -6,4 +7,11 @@ from photo.models import Photo
 
 @admin.register(Photo)
 class PhotoAdmin(admin.ModelAdmin, DynamicArrayMixin):
-    list_display = ["user", "description", "people"]
+    list_display = ["user", "description", "people", "get_preview_img"]
+    list_filter = ["user", "date"]
+
+    def get_preview_img(self, obj: Photo):
+        if obj.image:
+            return mark_safe(f"<img src = {obj.image.url} width=200>")
+
+    get_preview_img.short_description = "Image"
